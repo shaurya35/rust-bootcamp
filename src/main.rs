eafdc035bdbf727f38fa2e439a53174b3206887a
Shaurya Jha <shauryajha35@gmail.com>
2025-02-07 23:59:24 +0530

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    let ans: u32 = sum(1, 2);    
-    println!("{}", ans);
+    // let ans: u32 = sum(1, 2);    
+    // println!("{}", ans);
 
-    let even = is_even(10);
-    println!("{}", even);
+    // let even = is_even(10);
+    // println!("{}", even);
 
-    let name = String::from("Shaurya");
-    let v: Vec<i32> = vec![1,2,3];
+    // let name = String::from("Shaurya");
+    // let v: Vec<i32> = vec![1,2,3];
 
-    println!("{}", name);
-    println!("{:?}", v);
+    // println!("{}", name);
+    // println!("{:?}", v);
     // println!("Hello, world!"); // invoking a macro
 }
 
@@ -20,3 +20,8 @@ fn sum(a: u32 , b: u32 ) -> u32 {
 fn is_even(a: u32) -> bool {
     return a % 2 == 0
 }
+
+// fn create_str(){
+//     let str = String::from("Shaurya");
+
+// }
