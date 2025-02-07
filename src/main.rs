632d5056d46ebf3359f3d6bd58256194cf752993
Shaurya Jha <shauryajha35@gmail.com>
2025-02-07 21:19:34 +0530

    Rust init

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let ans: u32 = sum(1, 2);    
+    println!("{}", ans);
+
+    let even = is_even(10);
+    println!("{}", even);
+
+    let name = String::from("Shaurya");
+    let v: Vec<i32> = vec![1,2,3];
+
+    println!("{}", name);
+    println!("{:?}", v);
+    // println!("Hello, world!"); // invoking a macro
+}
+
+fn sum(a: u32 , b: u32 ) -> u32 {
+    return a+b;
+}
+
+fn is_even(a: u32) -> bool {
+    return a % 2 == 0
+}
